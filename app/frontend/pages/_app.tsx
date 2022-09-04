0560ae9e7574efb021f1144a38e2fad9fc18df93
Alexander Gusev <alxx.gusev@gmail.com>
2022-09-04 19:58:03 +0300

    Project page done

--- app/frontend/pages/_app.tsx
+++ app/frontend/pages/_app.tsx
@@ -1,9 +1,79 @@
 import '../styles/globals.sass';
 import type { AppProps } from 'next/app';
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { useClickAway } from 'react-use';
+
+import Link from 'next/link';
+import Discord from '../assets/icons/discord.svg';
+import Twitter from '../assets/icons/twitter.svg';
+import Telegram from '../assets/icons/telegram.svg';
+import Menu from '../assets/icons/menu.svg';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const mobileNavigation = useRef(null);
+
+  const links = {
+    telegram: 'https://contribute3.t.me/',
+    twitter: 'https://twitter.com/Contribute3HQ',
+    discord: 'https://discord.gg/hCqtJHKQvu',
+  };
+
+  useClickAway(mobileNavigation, () => {
+    setIsMenuOpen(false);
+  });
+
+  return (
+    <>
+      <header className="container mx-auto px-4 py-6 flex items-center gap-4 sm:gap-12">
+        <a href="/" className="font-mono text-primary font-bold text-xl">
+          <img src="./logo.svg" className="h-10" alt="contribute3 logo" />
+        </a>
+
+        <div className="flex-1" />
+
+        <nav className="hidden md:flex gap-8">
+          <Link href="/"><a className="text-white active:opacity-50">Home</a></Link>
+          <Link href="/projects/"><a className="text-white active:opacity-50">Explore projects</a></Link>
+          {/* <a className="text-white active:text-primary" href="#">Blog</a> */}
+        </nav>
+
+        <div className="hidden md:flex gap-4">
+          <a href={links.twitter} target="_blank"
+            className="text-white hover:text-emerald-400" rel="noreferrer"><Twitter className="h-6 w-6" alt="" /></a>
+          <a href={links.discord} target="_blank"
+            className="text-white hover:text-emerald-400" rel="noreferrer"><Discord className="h-6 w-6" alt="" /></a>
+          <a href={links.telegram} target="_blank"
+            className="text-white hover:text-emerald-400" rel="noreferrer"><Telegram className="h-6 w-6" alt="" /></a>
+        </div>
+
+        <div ref={mobileNavigation} className="md:hidden relative">
+          <button onClick={() => setIsMenuOpen(!isMenuOpen)} type="button" className="p-0">
+            <Menu />
+          </button>
+          <ul className={`${!isMenuOpen && 'hidden'} absolute z-10 divide-y divide-zinc-700 right-0 w-52 rounded-lg bg-zinc-800 text-left font-normal`}>
+            <div>
+              <li className="text-xs opacity-50 pt-4 pb-2 px-4">Navigation</li>
+              <li><a className="text-white active:opacity-50 py-2 px-4 block" href="/">Home</a></li>
+              <li><a className="text-white active:opacity-50 py-2 px-4 block" href="/projects/">Projects</a></li>
+            </div>
+            <div>
+              <li className="text-xs opacity-50 pt-4 pb-2 px-4">Join us</li>
+              <li><Link href={links.twitter}><a className="text-white active:opacity-50 py-2 px-4 block" target="_blank" rel="noreferrer">Twitter</a></Link></li>
+              <li><Link href={links.discord}><a className="text-white active:opacity-50 py-2 px-4 block" target="_blank" rel="noreferrer">Discord</a></Link></li>
+              <li><Link href={links.telegram}><a className="text-white active:opacity-50 py-2 px-4 block" target="_blank" rel="noreferrer">Telegram</a></Link></li>
+            </div>
+          </ul>
+        </div>
+      </header>
+
+      <Component {...pageProps} links={links} />
+
+      <footer className="container mx-auto text-center text-xs my-8 font-mono">
+          made by <a href="https://twitter.com/gssvv_" target="_blank" className="text-sky-500" rel="noreferrer">@gssvv_</a>
+      </footer>
+    </>
+  );
 }
 
 export default MyApp;
---
+++ app/frontend/pages/projects.tsx
@@ -0,0 +1,74 @@
+import { NextPage } from 'next';
+import React from 'react';
+import Head from 'next/head';
+
+import Discord from '../assets/icons/discord.svg';
+import Twitter from '../assets/icons/twitter.svg';
+import Telegram from '../assets/icons/telegram.svg';
+
+const Projects: NextPage = ({ links }: any) => (
+  <div>
+    <Head>
+      <title>Open-source Web3 Projects — Contribute3</title>
+      <meta name="description" content="Community where developers collaborate on open-source web3 projects
+to gain real-world experience, improve tech skills and build professional network." />
+      <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+    </Head>
+
+    <div className="container overflow-visible mx-auto px-4 my-16 sm:my-32 text-center relative">
+      <img src="./blurry-gradient.png" className="blurry-gradient blurry-gradient--md -top-1/4 -left-1/4" alt="" />
+      <img src="./blurry-gradient.png" className="blurry-gradient blurry-gradient--lg -right-20 sm:-right-48 top-0" alt="" />
+
+      <div className="relative">
+        <h1>Open-source web3 <br className="md:block hidden" /> projects to contribute</h1>
+        <p className="mt-6 opacity-70 sm:text-xl text-base leading-7 font-light">
+          Hand-picked open-source web3 projects to gain real-world experience,<br className="md:block hidden" />
+          imporve tech skills and build professional network.
+        </p>
+        <div className="flex sm:gap-6 gap-4 mt-10 justify-center flex-wrap">
+          <a href="https://discord.gg/hCqtJHKQvu" target="_blank" className="hover:no-underline" rel="noreferrer"><button className="bg-primary--darken">Join community</button></a>
+        </div>
+      </div>
+    </div>
+
+    <div className="blue-block mt-16 sm:mt-32 relative">
+      <div className="container mx-auto px-4 py-16 sm:py-32">
+        <h1 className="text-center">Stop here 🚧</h1>
+        <p className="mt-3 text-center opacity-50 text-md sm:text-lg">
+        This feature is still in progress. Follow us and get notified when this page is available.
+        </p>
+
+        <div className="flex flex-wrap gap-4 mt-8 sm:mt-16 justify-center">
+          <a target="_blank" href={links.twitter} className="block text-center hover:no-underline" rel="noreferrer">
+            <button className="text-sky-500">
+              <Twitter height={20} />
+            Twitter
+            </button>
+          </a>
+
+          <a target="_blank" href={links.discord} className="block text-center hover:no-underline" rel="noreferrer">
+            <button className="text-indigo-400">
+              <Discord height={20} />
+            Discord
+            </button>
+          </a>
+
+          <a target="_blank" href={links.telegram} className="block text-center hover:no-underline" rel="noreferrer">
+            <button className="text-sky-400">
+              <Telegram height={20} />
+            Telegram
+            </button>
+          </a>
+        </div>
+
+        <p className="mt-32 mb-4 text-center opacity-50 text-md sm:text-lg">
+          P.S. Here is how it will look like:
+        </p>
+
+        <img className="rounded-3xl" src="/projects-preview.png" alt="projects preview" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Projects;
